454a556cb536b44918bbea0b1bd2c5a9e4fbd25a
Edward Kwartler <edwardkwartler@Edwards-MacBook-Air.local>
2023-03-23 16:04:50 -0400

    try again

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -87,7 +87,6 @@
 		on:submit|preventDefault={() => handleSubmit()}
 	>
 		<input type="text" class="input input-bordered w-full" bind:value={query} />
-		<!-- <button type="submit" class="btn btn-accent"> Send </button> -->
-    <button type="submit" class="btn btn-accent"> Send </button>
+	  <button type="submit" class="btn btn-accent"> Send </button>
 	</form>
 </div>
